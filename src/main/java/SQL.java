ce37a0f4c1df08df97b9ccd9dddcc761b95a0fb1
Archishmaan Peyyety <archishou@gmail.com>
2017-11-21 23:49:15 -0500

    sql controller

--- src/main/java/SQL.java
+++ src/main/java/SQL.java
@@ -15,28 +15,10 @@ public class SQL {
     private Connection connection = null;
     private Statement stmt = null;
     public SQLController sqlController = new SQLController();
-    //public List<String> userName, schoolName, author, bookName;
-    //public List<Integer> userId, checkedOutBooks, limitOfBooks, forirghschoolId, bookId, schoolId, transactionId, forirghUserId, forirghBookId, fine, tLim, sLim ;
-    //public List<Boolean> userStatus;
+
     private Date utilDate = new Date();
     public SQL () {
-        /*userId = new ArrayList<>();
-        userName = new ArrayList<>();
-        checkedOutBooks = new ArrayList<>();
-        limitOfBooks = new ArrayList<>();
-        forirghschoolId = new ArrayList<>();
-        userStatus = new ArrayList<>();
-        bookId = new ArrayList<>();
-        author = new ArrayList<>();
-        bookName= new ArrayList<>();
-        tLim= new ArrayList<>();
-        sLim = new ArrayList<>();
-        schoolName = new ArrayList<>();
-        fine = new ArrayList<>();
-        schoolId = new ArrayList<>();
-        transactionId = new ArrayList<>();
-        forirghBookId = new ArrayList<>();
-        forirghUserId = new ArrayList<>();*/
+
     }
     public enum Database {
         USERS("users");
