b85706548c45d5cb5ce552397b6e6ac5c73dc03a
Archishmaan Peyyety <archishou@gmail.com>
2018-01-04 20:39:21 -0500

    ready to deploy

--- src/main/java/DetailFineView.java
+++ src/main/java/DetailFineView.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DetailFineView extends Grid {
+public class DetailFineView extends Grid {//
     private Grid<DetailFine> detailFineGrid;
     private SQL sql;
     public DetailFineView () {
