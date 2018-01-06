45012e9483233143c454ef476ebf6f34443a2f5b
Archishmaan Peyyety <archishou@gmail.com>
2018-01-06 16:27:45 -0500

    added strings for help

--- src/main/java/HelpView.java
+++ src/main/java/HelpView.java
@@ -2,5 +2,18 @@ import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
-public class HelpView extends VerticalLayout {
+public class HelpView extends HelpDesighn {
+    public HelpView () {
+        desc.setSizeFull();
+        desc.setValue(strings.users);
+        user.addClickListener(clickEvent -> {
+           desc.setValue(strings.users);
+        });
+        book.addClickListener(clickEvent -> {
+            desc.setValue(strings.books);
+        });
+        report.addClickListener(clickEvent -> {
+            desc.setValue(strings.reports);
+        });
+    }
 }
